a6b4b1d62ac14cf647439219352a6e4f16d38639
Barış Avtan <baris.avtan@arvento.com>
2023-03-04 17:18:21 +0300

    add photo services.

--- Services/PotoStock/Course.Services.PhotoStock/Program.cs
+++ Services/PotoStock/Course.Services.PhotoStock/Program.cs
@@ -15,6 +15,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseStaticFiles();//dosyalara public olarak eri?ebilmek için
 
 app.UseAuthorization();
 
